e3f2e02f51f019d6a186e2e6ea205ada6e629c73
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-13 11:13:59 -0400

    javascript5

--- payment.js
+++ payment.js
@@ -8,5 +8,5 @@ number1 = prompt("Please enter your pay per hour");
 number1 = parseInt(number1); // Convert to type 'int' 
 number2 = prompt("Please enter the hours you have worked this week"); 
 number2 = parseInt(number2); // Convert to type 'int' 
-numbe3 = prompt("Please enter "); 
+numbe3 = prompt("Please enter any overtime hours  "); 
 number31 = parseInt(number3); // Convert to type 'int' 
