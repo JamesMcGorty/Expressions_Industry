aa26642d5ce82d474d272bddc23edd6cecde25b8
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-13 11:11:31 -0400

    javascript3

--- payment.js
+++ payment.js
@@ -4,5 +4,7 @@
 function averagecalc() 
 { 
 
-number1 = prompt("Please enter pay per hour"); 
+number1 = prompt("Please enter your pay per hour"); 
 number1 = parseInt(number1); // Convert to type 'int' 
+number2 = prompt("Please enter your hourly wage"); 
+number2 = parseInt(number1); // Convert to type 'int' 
